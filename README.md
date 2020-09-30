2ee01cdf046abcf69a842f8fb323231634dd3238
polatpoyraz-1 <polat64polat34@gmail.com>
2020-10-01 01:37:51 +0300

    finish prototype.js

---
+++ README.md
@@ -0,0 +1,7 @@
+# Prototype.js
+
+Prototype.js ile prototipleriniz daha rahat yazın.
+Prototip yazarken yeni html dosyaları açıp tek tek girip çıkmakla uğraşmayın,
+Prototip.js ile tek sayfa üzerinden tüm prototiplerinize anında ulaşın.
+
+Kullanımı ve dosya yapısı başlatdığınız'da karşınıza çıkmaktadır.
---
+++ server.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const app = express()
+app.use('/styles', express.static(path.join(__dirname, 'styles')))
+
+fs.readdir('./public', (err, files) => {
+    for (let i in files) {
+        app.get(`/${files[i].replace('.html', '')}`, (req, res) => {
+            res.sendFile(path.join(__dirname, `/public/${files[i].replace('.html', '')}.html`))
+        })
+    }
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/main.html'))
+})
+
+app.listen(8000)
