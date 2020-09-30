f00559b97f8b30772aeac4e3d5680b0f5338c601
polatpoyraz-1 <polat64polat34@gmail.com>
2020-10-01 02:56:43 +0300

    add assets

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path')
 const express = require('express')
 const app = express()
 app.use('/styles', express.static(path.join(__dirname, 'styles')))
+app.use('/assets', express.static(path.join(__dirname, 'assets')))
 
 fs.readdir('./public', (err, files) => {
     for (let i in files) {
